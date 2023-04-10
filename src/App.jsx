c91301b07e0a7ac1bdcad4e8d69354560e773338
moktubat <shadhinreja@gmail.com>
2023-04-11 04:09:01 +0600

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
   return (
     <div>
       <Header/>
-      <Home/>
       <Statistics/>
       <Outlet/>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,44 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import coverImage from '../assets/images/hero.png';
 
 const Home = () => {
   return (
-    <div className='my-container flex flex-col items-center justify-between lg:flex-row'>
+    <div className='bg-indigo-100 my-container flex flex-col items-center justify-between lg:flex-row'>
       {/* Text Content */}
       <div className='mb-10 lg:max-w-lg  lg:pr-5 lg:mb-0'>
         <div className='max-w-xl mb-6 lg:mt-8'>
           <div>
-            <p className='badge'>On Sale!</p>
           </div>
-          <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
-            A reader lives a <br className='hidden md:block' /> thousand lives{' '}
-            <span className='inline-block text-blue-400'>before he dies</span>
-          </h2>
-          <p className='text-base text-gray-700 md:text-lg'>
-            Books are a uniquely portable magic. Books serve to show a man that
-            those original thoughts of his aren’t very new after all. The man
-            who does not read good books is no better than the man who can’t.
+          <h1 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-7xl sm:leading-none'>
+          One Step <br className='hidden md:block' /> Closer To Your{' '} <br className='hidden md:block' />
+            <span className='inline-block text-blue-400'>Dream Job</span>
+          </h1>
+          <p className='text-base text-gray-500 md:text-lg'>
+          Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
           </p>
         </div>
         <div className='flex flex-col items-center md:flex-row'>
           <Link to='/books' className='btn md:w-auto md:mr-4'>
             <div className='inline-flex items-center justify-center w-full h-full'>
-              <p className='mr-3'>Visit Store</p>
+              <p className='mr-3'>Get Started</p>
             
             </div>
-          </Link>
-          <Link
-            to='/about'
-            className='inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-blue-700'
-          >
-            Learn More
           </Link>
         </div>
       </div>
       {/* Lottie Animation */}
-      <div className='relative lg:w-1/2 '>
+      <div className='relative lg:w-1/2 w-1/2'>
         <div className='w-full lg:w-4/5 lg:ml-auto h-56  sm:h-96'>
-          
+            <img src={coverImage} alt="" />
         </div>
       </div>
     </div>  
